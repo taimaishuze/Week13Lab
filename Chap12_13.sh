5b646e727bcc69fd2dca3a3d29761a626416fbcc
taimaishuze <35374281+taimaishuze@users.noreply.github.com>
2018-04-11 09:26:56 -0500

    added chapter 13 notes

--- Chap12_13.sh
+++ Chap12_13.sh
@@ -4,9 +4,14 @@
 # Week 13 CSCI 2461 Computer Networking 2 - Linux
 # Chapter 12 and 13
 
+echo "Note: None of the following lines will run since they are all proceeded with echo"
+echo "user must provide additional information in order to modify the scripts."
+echo ""
+echo "Chapter 12 notes"
+echo ""
 echo "The following command copies files from one node to another node on the same network."
 echo "python -m SimpleHTTPServer"
-
+echo ""
 echo "Use the following command to move an entire directory "
 echo "scp -r"
 echo "or better yet, use tar in a pipeline:"
@@ -22,3 +27,19 @@ echo "The following command will exclude files and directories during transfer o
 echo "We will use src and host as examples.  We will exclude anything with .git"
 echo ""
 echo "rsync -a --exclude=.git src host:"
+echo ""
+echo "Use rsync to limit bandwidth on your network with the following command:
+echo "dir host and destination_dir must be specified by user"
+echo ""
+echo "rsync --bwlimit=10000 -a dir host: destination_dir"
+echo ""
+echo "Chapter 13 notes"
+echo ""
+echo "Essential goals to keep in mind when creating startup files for other users"
+echo " * Simplicity
+echo " * Readability
+echo ""
+echo "Good reasons for changing startup files:"
+echo " * You want to change the default prompt"
+echo " * You need to accommodate some critical locally installed software."
+echo " * Your existing startup files are broken"
